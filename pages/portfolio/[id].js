3e404736e0a07504ab5dfe5d82086ced0ab5377f
GenaPlem <shainkill1337@gmail.com>
2024-11-08 10:49:51 +0000

    Add router to use dynamic query

--- pages/portfolio/[id].js
+++ pages/portfolio/[id].js
@@ -1,7 +1,14 @@
+import { useRouter } from "next/router";
+
 export default function PortfolioProjectPage() {
+  const router = useRouter();
+
+  const dynamicQuery = router.query.id;
+
   return (
     <div>
       <h1>The Portfolio Project Page</h1>
+      <p>{dynamicQuery}</p>
     </div>
   );
 }
